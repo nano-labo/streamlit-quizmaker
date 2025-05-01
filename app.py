7291736deb633204f008c981f5a7eb7d24c7e64b
Narita <nnarita@alixpartners.com>
2025-05-01 11:43:33 +0900

    first commit.

--- app.py
+++ app.py
@@ -218,9 +218,9 @@ if st.session_state.answered and st.button("次の問題へ"):
     if st.session_state.quiz_index >= len(quiz_data):
         st.write("🎉 クイズ終了！")
         st.write(f"あなたのスコア: {st.session_state.score} / {len(quiz_data)}")
-        display_menu()
         st.session_state.quiz_index = 0
         st.session_state.quiz_index
+        display_menu()
         st.stop()
     else:
         st.rerun()
