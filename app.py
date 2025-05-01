c0b6198fae791792f0c43ed82aaec05952db4da8
Narita <nnarita@alixpartners.com>
2025-05-01 11:45:48 +0900

    first commit.

--- app.py
+++ app.py
@@ -220,6 +220,7 @@ if st.session_state.answered and st.button("次の問題へ"):
         st.write(f"あなたのスコア: {st.session_state.score} / {len(quiz_data)}")
         st.session_state.quiz_index = 0
         st.session_state.quiz_index
+        st.session_state.genre = None
         display_menu()
         st.stop()
     else:
